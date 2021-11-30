40320eeb046b63619b650ee103d11736b129290e
maxim920 <90177561+maxim920@users.noreply.github.com>
2021-11-30 22:43:50 +0200

    Add files via upload

---
+++ zavdrandom.py
@@ -0,0 +1,7 @@
+import random
+
+dog = ["Чорна","Біла","Сіра"]
+price = [50,100,200]
+poroda = ["породиста","без породи"]
+
+print(random.choice(dog),"собака",random.choice(price), "грн",random.choice(poroda))
